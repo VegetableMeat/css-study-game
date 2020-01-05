f94d7bfda7a8fb4ab97e6d1c7bc78b911b618dcd
ra-i-to <rightning83@gmail.com>
2020-01-06 02:50:05 +0900

    right

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ $(function(){
     const gameCountMax = 3;
     var gameCountText = gameCount + "/" + gameCountMax;
 
+    //１レベルごとに必要な経験値
+    const levelUpExp = 1000;
+    //合計獲得経験値（仮データ）
+    var totalExp = 1280;
+
     //スタートボタン
     $(".start-button button").click(function(){
         $(".start-before-area").css("display","none");
@@ -16,9 +21,13 @@ $(function(){
         
         if(clearFlg === true){
             if(gameCount === gameCountMax){
+                //ゲーム結果画面出力
                 $(".modal-result").css("display", "block");
                 $("#main-body").css("filter", "blur(8px)");
+
+                
             }else{
+                //ステージクリア画面出力
                 $(".modal").css("display", "block");
                 $("#main-body").css("filter", "blur(8px)");
             }
@@ -54,11 +63,66 @@ $(function(){
         $(".code-area .css").css("display", "block");
     });
 
-    //１レベルごとに必要な経験値
-    const levelUpExp = 1000;
-    //合計獲得経験値（仮データ）
-    var totalExp = 1280;
-    var nextLevelExp = totalExp % levelUpExp;
-    var expBarWidth = levelUpExp / nextLevelExp * 10;
-    $(".exp-bar").css("width", expBarWidth+"%");
+    
+    //プロフィール画面　経験ゲージアニメーション
+    nextLevelExp = totalExp % levelUpExp;
+    expBarWidth =  nextLevelExp / levelUpExp * 100;
+    $("#profile .exp-bar").css("width", expBarWidth + "%");
+
+ 
+    //ゲーム画面　ゲーム結果モーダルウィンドウ　経験値ゲージアニメーション
+    var getExp = 4200;
+    var diffExp = getExp;
+    var beforeTotalExp = totalExp;
+    var beforeLevel = Math.floor(beforeTotalExp / 1000) * 1;
+    var afterTotalExp = totalExp + getExp;
+    var afterLevel = Math.floor(afterTotalExp / 1000) * 1;
+    nextLevelExp = beforeTotalExp % levelUpExp;
+    expBarWidth =  nextLevelExp / levelUpExp * 100;
+    $("#game .exp-bar").css("transition", "0s");
+    $("#game .exp-bar").css("width", expBarWidth + "%");
+    setTimeout(function(){
+        $("#game .exp-bar").css("transition", "width 1s ease-in-out");
+    }, 500);
+    setTimeout(function(){
+        if(beforeLevel != afterLevel){
+            for(i = afterLevel - beforeLevel; i > 0; i--){
+                
+                $("#game .exp-bar").css("width", "100%");
+                $(".level-up-message").css({"transition" : "0.6s ease-in-out"});
+                setTimeout(function(){
+                    $(".level-up-message").css({"opacity" : "1", "transform" : "translateY(-56px)"});
+                }, 1000);
+                setTimeout(function(){
+                    $(".level-up-message").css({"transition" : "0s ease-in-out", "opacity" : "0", "transform" : "translateY(-120px)"});
+                    $("#game .exp-bar").css("transition", "0s");
+                    $("#game .exp-bar").css("width", "0");
+                }, 3000);
+                if(i >= 2){
+                    setTimeout(function(){
+                        $("#game .exp-bar").css("transition", "width 1s ease-in-out");
+                        $("#game .exp-bar").css("width", "100%");
+                    }, 3200);
+                    setTimeout(function(){
+                        $(".level-up-message").css({"transition" : "0.6s ease-in-out", "opacity" : "1", "transform" : "translateY(-56px)"});
+                    }, 4200);
+                }else{
+                    setTimeout(function(){
+                        $(".level-up-message").css({"transition" : "0s ease-in-out", "opacity" : "0", "transform" : "translateY(-120px)"});
+                        $("#game .exp-bar").css("transition", "0s");
+                        $("#game .exp-bar").css("width", "0");
+                    }, 6000);
+                    setTimeout(function(){
+                        $("#game .exp-bar").css("transition", "width 1s ease-in-out");
+                        $("#game .exp-bar").css("width", expBarWidth + "%");
+                    }, 6200);
+                }
+            }
+        }else{
+            nextLevelExp = afterTotalExp % levelUpExp;
+            expBarWidth =  nextLevelExp / levelUpExp * 100;
+            $("#game .exp-bar").css("width", expBarWidth + "%");
+        }
+    }, 1000);
+        
 });
