0e2a92a4790629cb08f8ef83acb1925409d82e38
taniwhy <tanitani628@gmail.com>
2020-01-06 05:38:02 +0900

    APIアクセス用JSファイルを追加

--- api.js
+++ api.js
@@ -1,52 +1,55 @@
 $(function () {
-    // 登録ボタン
+    // アクセス先WebAPI URL
+    const apiUrl = "http://126.60.26.165:80";
+
+    // 登録ボタン押下時に実行
     $(".register-button button").click(function () {
-        // 各フィールドから値を取得してJSONデータを作成
-        var data = {
+        // 各フォームから値を取得してJSONデータを作成
+        let data = {
             user_id: $('[type=text]').val(),
             password: $('[type=password]').val()
         };
 
-        // 通信実行
+        // WebAPIアクセス実行
         $.ajax({
-            type: "post",                // method = "POST"
-            url: "http://126.60.26.165:80/signup",        // POST送信先のURL
-            data: JSON.stringify(data),  // JSONデータ本体
-            contentType: 'application/json', // リクエストの Content-Type
-            dataType: "json",           // レスポンÏスをJSONとしてパースする
-            success: function (json_data) {   // 200 OK時
+            type: "post",
+            url: `${apiUrl}/signup`,
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            dataType: "json",
+            success: function (json_data) {
                 console.log(json_data)
             },
             error: function (json_data) {
                 console.log(json_data)
             },
-            complete: function () {      // 成功・失敗に関わらず通信が終了した際の処理
+            complete: function () {
             }
         });
     });
 
-    // ログインボタン
+    // ログインボタン押下時に実行
     $(".login-button button").click(function () {
-        // 各フィールドから値を取得してJSONデータを作成
-        var data = {
+        // 各フォームから値を取得してJSONデータを作成
+        let data = {
             user_id: $('[type=text]').val(),
             password: $('[type=password]').val()
         };
 
-        // 通信実行
+        // WebAPIアクセス実行
         $.ajax({
-            type: "post",                // method = "POST"
-            url: "http://126.60.26.165:80/login",        // POST送信先のURL
-            data: JSON.stringify(data),  // JSONデータ本体
-            contentType: 'application/json', // リクエストの Content-Type
-            dataType: "json",           // レスポンÏスをJSONとしてパースする
-            success: function (json_data) {   // 200 OK時
+            type: "post",
+            url: `${apiUrl}/login`,
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            dataType: "json",
+            success: function (json_data) {
                 console.log(json_data)
             },
             error: function (json_data) {
                 console.log(json_data)
             },
-            complete: function () {      // 成功・失敗に関わらず通信が終了した際の処理
+            complete: function () {
             }
         });
     });
