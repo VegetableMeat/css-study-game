0bf5f48812628f6cf21477489f77812d3ce19e4d
ra-i-to <rightning83@gmail.com>
2020-01-07 22:19:28 +0900

    right

--- script.js
+++ script.js
@@ -70,15 +70,18 @@ $(function(){
     });
 
     
+
+
     //プロフィール画面　経験ゲージアニメーション
     nextLevelExp = totalExp % levelUpExp;
     expBarWidth =  nextLevelExp / levelUpExp * 100;
     $("#profile .exp-bar").css("width", expBarWidth + "%");
 
  
-    
+
+
+    //ゲーム画面　ゲーム結果モーダルウィンドウ　経験値ゲージアニメーション
     function getExpAnimation(getExp){
-        //ゲーム画面　ゲーム結果モーダルウィンドウ　経験値ゲージアニメーション
         var beforeTotalExp = totalExp;
         var beforeLevel = Math.floor(beforeTotalExp / 1000) * 1;
         var afterTotalExp = totalExp + getExp;
@@ -135,5 +138,132 @@ $(function(){
         $("#game .level-area .total-exp span").text(totalExp + getExp);
         $("#game .level-area .next-exp span").text(nextLevelExp);
     }
+
+
+
+    //ゲーム中の画面遷移などによるゲーム中断処理
+    var linkUrl;
+    $("#game .profile a").click(function(e){
+        linkUrl = $(this).attr('href');
+        discontinue(e,linkUrl);
+    });
+    $("#game .exit a").click(function(e){
+        linkUrl = $(this).attr('href');
+        discontinue(e,linkUrl);
+    });
+    $("#game header .center a").click(function(e){
+        linkUrl = $(this).attr('href');
+        discontinue(e,linkUrl);
+    });
+    function discontinue(e,linkUrl){
+        e.preventDefault();
+        swal({
+            title: "注意",
+            text: "画面を離れるとゲームが中断され、ゲームの再開や経験値の獲得ができません。\nよろしいですか？",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+            })
+            .then((move) => {
+            if (move) {
+                action();
+            } else {
+                
+            }
+            });
+        
+        function action() {
+            location.href = linkUrl;
+        }
+        setTimeout(action,999999);
+    }
+
+
+    const errorMessage = {
+        "existId" : "*既に使用されているユーザーIDが入力されています。",
+        "retypeDismatchPw" : "*再入力のパスワードが異なっています。",
+        "unregisterId" : "*入力されたユーザーIDは登録されていません。",
+        "diffPw" : "*パスワードが違います。",
+        "badId" : "*ユーザーIDに不正な文字が使用されています。英数字のみで入力してください。",
+        "longId" : "*ユーザーIDの文字数が超過しています。〜文字以内で入力してください。",
+        "shortId" : "*ユーザーIDの文字数が少なすぎます。～文字以上で入力してください。",
+        "badPw" : "*パスワードに不正な文字が使用されています。英数字のみで入力してください。",
+        "longPw" : "*パスワードの文字数が超過しています。〜文字以内で入力してください。",
+        "shortPw" : "*パスワードの文字数が少なすぎます。～文字以上で入力してください。"
+    };
+    const errorInput = {
+        "existId" : "user-id",
+        "retypeDismatchPw" : "user-pw-2",
+        "unregisterId" : "user-id",
+        "diffPw" : "user-pw-1",
+        "badId" : "user-id",
+        "longId" : "user-id",
+        "shortId" : "user-id",
+        "badPw" : "user-pw-1",
+        "longPw" : "user-pw-1",
+        "shortPw" : "user-pw-1"
+    }
+    var errorFlg = {
+        "existId" : false,
+        "retypeDismatchPw" : false,
+        "unregisterId" : false,
+        "diffPw" : false,
+        "badId" : false,
+        "longId" : false,
+        "shortId" : false,
+        "badPw" : false,
+        "longPw" : false,
+        "shortPw" : false
+    };
+    var allErrorFlg = false;
+    var errorMessageHtml = "";
+
+    //登録ボタン押下後の処理
+    $(".register-button").click(function(){
+        //初期化
+        errorMessageHtml = "";
+        allErrorFlg = false;
+        for(key in errorFlg){
+            errorFlg[key] = false;
+            $("." + errorInput[key] + " input").css("border", "solid 1px white");
+        }
+
+        //入力チェック
+        
+        //テストエラーフラグ
+        errorFlg["badId"] = true;
+        errorFlg["longPw"] = true;
+
+        for(key in errorFlg){
+            if(errorFlg[key] === true){
+                allErrorFlg = true;
+                errorMessageHtml = errorMessageHtml + errorMessage[key] + "\n\n";
+
+                $("." + errorInput[key] + " input").css("border", "solid 1px red");
+            }
+        }
+        if(allErrorFlg === false){
+            //エラーがなかった場合の処理
+            window.location.href = "front.html";
+        }else{
+            //エラーがあった場合の処理
+            //モーダルウィンドウでエラーメッセージ表示
+            swal({
+                title: "Error!",
+                text: errorMessageHtml,
+                icon: "warning"
+                })
+                .then((ok) => {
+                if (ok) {
+                    
+                } else {
+                    
+                }
+            });
+
+            
+        }
+    });
+
 });
 
