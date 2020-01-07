ecd6d0a55aed71366c929a5cb1d560487abc7f23
ra-i-to <rightning83@gmail.com>
2020-01-07 11:54:54 +0900

    right

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(function(){
     //１レベルごとに必要な経験値
     const levelUpExp = 1000;
     //合計獲得経験値（仮データ）
-    var totalExp = 1280;
+    var totalExp = 1800;
 
     //スタートボタン
     $(".start-button button").click(function(){
@@ -23,13 +23,18 @@ $(function(){
             if(gameCount === gameCountMax){
                 //ゲーム結果画面出力
                 $(".modal-result").css("display", "block");
-                $("#main-body").css("filter", "blur(8px)");
+                $("#main-body").css("filter", "blur(10px)");
+                $(".game-header").css("filter", "blur(10px)");
+
+                var getExp = 1500;
+                getExpAnimation(getExp);
 
                 
             }else{
                 //ステージクリア画面出力
                 $(".modal").css("display", "block");
-                $("#main-body").css("filter", "blur(8px)");
+                $("#main-body").css("filter", "blur(10px)");
+                $(".game-header").css("filter", "blur(10px)");
             }
         }else{
             //エラーメッセージ出力処理
@@ -44,6 +49,7 @@ $(function(){
         $(".game-count span").text(gameCountText);
         $(".modal").css("display", "none");
         $("#main-body").css("filter", "none");
+        $(".game-header").css("filter", "none");
     });
 
     $(".html-tab").click(function(){
@@ -70,59 +76,64 @@ $(function(){
     $("#profile .exp-bar").css("width", expBarWidth + "%");
 
  
-    //ゲーム画面　ゲーム結果モーダルウィンドウ　経験値ゲージアニメーション
-    var getExp = 4200;
-    var diffExp = getExp;
-    var beforeTotalExp = totalExp;
-    var beforeLevel = Math.floor(beforeTotalExp / 1000) * 1;
-    var afterTotalExp = totalExp + getExp;
-    var afterLevel = Math.floor(afterTotalExp / 1000) * 1;
-    nextLevelExp = beforeTotalExp % levelUpExp;
-    expBarWidth =  nextLevelExp / levelUpExp * 100;
-    $("#game .exp-bar").css("transition", "0s");
-    $("#game .exp-bar").css("width", expBarWidth + "%");
-    setTimeout(function(){
-        $("#game .exp-bar").css("transition", "width 1s ease-in-out");
-    }, 500);
-    setTimeout(function(){
-        if(beforeLevel != afterLevel){
-            for(i = afterLevel - beforeLevel; i > 0; i--){
-                
-                $("#game .exp-bar").css("width", "100%");
-                $(".level-up-message").css({"transition" : "0.6s ease-in-out"});
-                setTimeout(function(){
-                    $(".level-up-message").css({"opacity" : "1", "transform" : "translateY(-56px)"});
-                }, 1000);
-                setTimeout(function(){
-                    $(".level-up-message").css({"transition" : "0s ease-in-out", "opacity" : "0", "transform" : "translateY(-120px)"});
-                    $("#game .exp-bar").css("transition", "0s");
-                    $("#game .exp-bar").css("width", "0");
-                }, 3000);
-                if(i >= 2){
+    
+    function getExpAnimation(getExp){
+        //ゲーム画面　ゲーム結果モーダルウィンドウ　経験値ゲージアニメーション
+        var beforeTotalExp = totalExp;
+        var beforeLevel = Math.floor(beforeTotalExp / 1000) * 1;
+        var afterTotalExp = totalExp + getExp;
+        var afterLevel = Math.floor(afterTotalExp / 1000) * 1;
+        nextLevelExp = levelUpExp - afterTotalExp % levelUpExp;
+        expBarWidth =  100 - nextLevelExp / levelUpExp * 100;
+        $("#game .exp-bar").css("transition", "0s");
+        $("#game .exp-bar").css("width", expBarWidth + "%");
+        setTimeout(function(){
+            $("#game .exp-bar").css("transition", "width 1s ease-in-out");
+        }, 500);
+        setTimeout(function(){
+            if(beforeLevel != afterLevel){
+                for(i = afterLevel - beforeLevel; i > 0; i--){
+                    
+                    $("#game .exp-bar").css("width", "100%");
+                    $(".level-up-message").css({"transition" : "0.6s ease-in-out"});
                     setTimeout(function(){
-                        $("#game .exp-bar").css("transition", "width 1s ease-in-out");
-                        $("#game .exp-bar").css("width", "100%");
-                    }, 3200);
-                    setTimeout(function(){
-                        $(".level-up-message").css({"transition" : "0.6s ease-in-out", "opacity" : "1", "transform" : "translateY(-56px)"});
-                    }, 4200);
-                }else{
+                        $(".level-up-message").css({"opacity" : "1", "transform" : "translateY(-56px)"});
+                    }, 1000);
                     setTimeout(function(){
                         $(".level-up-message").css({"transition" : "0s ease-in-out", "opacity" : "0", "transform" : "translateY(-120px)"});
                         $("#game .exp-bar").css("transition", "0s");
                         $("#game .exp-bar").css("width", "0");
-                    }, 6000);
-                    setTimeout(function(){
-                        $("#game .exp-bar").css("transition", "width 1s ease-in-out");
-                        $("#game .exp-bar").css("width", expBarWidth + "%");
-                    }, 6200);
+                    }, 3000);
+                    if(i >= 2){
+                        setTimeout(function(){
+                            $("#game .exp-bar").css("transition", "width 1s ease-in-out");
+                            $("#game .exp-bar").css("width", "100%");
+                        }, 3200);
+                        setTimeout(function(){
+                            $(".level-up-message").css({"transition" : "0.6s ease-in-out", "opacity" : "1", "transform" : "translateY(-56px)"});
+                        }, 4200);
+                    }else{
+                        setTimeout(function(){
+                            $(".level-up-message").css({"transition" : "0s ease-in-out", "opacity" : "0", "transform" : "translateY(-120px)"});
+                            $("#game .exp-bar").css("transition", "0s");
+                            $("#game .exp-bar").css("width", "0");
+                        }, 6000);
+                        setTimeout(function(){
+                            $("#game .exp-bar").css("transition", "width 1s ease-in-out");
+                            $("#game .exp-bar").css("width", expBarWidth + "%");
+                        }, 6200);
+                    }
                 }
+            }else{
+                nextLevelExp = levelUpExp - afterTotalExp % levelUpExp;
+                expBarWidth =  nextLevelExp / levelUpExp * 100;
+                $("#game .exp-bar").css("width", expBarWidth + "%");
             }
-        }else{
-            nextLevelExp = afterTotalExp % levelUpExp;
-            expBarWidth =  nextLevelExp / levelUpExp * 100;
-            $("#game .exp-bar").css("width", expBarWidth + "%");
-        }
-    }, 1000);
-        
+        }, 1000);
+
+        $("#game .level-area .get-exp span").text(getExp);
+        $("#game .level-area .total-exp span").text(totalExp + getExp);
+        $("#game .level-area .next-exp span").text(nextLevelExp);
+    }
 });
+
